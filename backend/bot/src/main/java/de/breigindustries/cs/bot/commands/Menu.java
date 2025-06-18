e63dfb2ff64add43582845c84c32d6ec22024955
BreigiMM <130593079+BreigiMM@users.noreply.github.com>
2025-06-18 21:44:40 +0200

    Remove unnecessary imports

--- backend/bot/src/main/java/de/breigindustries/cs/bot/commands/Menu.java
+++ backend/bot/src/main/java/de/breigindustries/cs/bot/commands/Menu.java
@@ -1,8 +1,5 @@
 package de.breigindustries.cs.bot.commands;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import io.github.cdimascio.dotenv.Dotenv;
 import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
@@ -12,7 +9,6 @@ import net.dv8tion.jda.api.requests.restaction.interactions.ReplyCallbackAction;
 public class Menu {
 
     private static Dotenv dotenv = Dotenv.configure().load();
-    private static Logger logger = LoggerFactory.getLogger(Menu.class);
     
     public static void showMenu(SlashCommandInteractionEvent event) {
         // Create personalized menu message
